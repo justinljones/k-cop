4c2ebd20fcbdaf27de7856446bb152ef1a6206d8
JJones <jjones@brysontech.com>
2017-05-15 07:21:56 -0400

    Challenge 2 Attempting to change the background color and font color of the body and h1 elements.

--- js/site.js
+++ js/site.js
@@ -6,3 +6,15 @@ title.appendChild(titleText);
 head.appendChild(title);
 document.getElementsByTagName('html')[0].appendChild(head);
 
+//Creating Body content for K-COP
+var body=document.createElement("body");
+var bhead=document.createElement("h1");
+var bheadText=document.createTextNode("The Start of Something Big");
+bhead.appendChild(bheadText);
+body.appendChild(bhead);
+document.getElementsByTagName('html')[0].appendChild(body);
+
+//Changing Page styling
+document.getElementsByTagname("body")[0].style.background="black";
+document.getElementsByTagname("h1").style.color=white;
+
