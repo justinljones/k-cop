bf5b44a9281de8af7db15fe10a8df024e3e6222a
JJones <jjones@brysontech.com>
2017-05-14 22:59:48 -0400

    Challenge 2 Modified the js file to create a head and title element. Also to create text to go into the title element. Finally, appended the head to the html tag to build a title for page.

--- js/site.js
+++ js/site.js
@@ -1,5 +1,8 @@
 //Creating a title for K-COP
+var head=document.createElement("head");
 var title=document.createElement("title");
 var titleText = document.createTextNode("Welcome to KCOP");
 title.appendChild(titleText);
+head.appendChild(title);
+document.getElementsByTagName('html')[0].appendChild(head);
 
